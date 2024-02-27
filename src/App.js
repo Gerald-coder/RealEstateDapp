15d266d4923794ff3292dedc5f968079eeabf73b
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-02-27 12:10:01 +0100

    program to switch account automatically

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ function App() {
     // console.log(provider);
 
     // automatically changing account
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
   };
   console.log(account);
 
